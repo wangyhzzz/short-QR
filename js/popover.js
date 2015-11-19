f1d751ee5bc1bdf6e654113923fecfb02d6be4a6
wangyh <wangyihai@HiHiMBP.local>
2015-11-19 21:53:31 +0800

    error handle
    获取短网址失败的话,就显示原始地址的二维码

--- js/popover.js
+++ js/popover.js
@@ -2,10 +2,19 @@ safari.application.addEventListener("popover",fff,true);
 function fff(event){
     if(event.target.identifier !== "ppop")return;
     try{
+        var long_url = safari.application.activeBrowserWindow.activeTab.url;
         $.ajax({
-            url:'http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long='+safari.application.activeBrowserWindow.activeTab.url,
+            url:'http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long='+long_url,
             async:false,
             data:null,
+            error:function(){
+                $('#qrcode-box').empty();
+                $('#qrcode-box').qrcode({
+                    width: 200,
+                    height: 200,
+                    text: long_url
+                });
+            }
             success:function(re){
                 $('#qrcode-box').empty();
                 $('#qrcode-box').qrcode({
