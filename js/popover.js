f887295d8c1aed4d7e1c72e4a8e59cba438f176b
wangyh <wangyihai@HiHiMBP.local>
2015-11-20 23:17:30 +0800

    error handle
    重新处理错误处理

--- js/popover.js
+++ js/popover.js
@@ -1,13 +1,27 @@
 safari.application.addEventListener("popover",fff,true);
 function fff(event){
     if(event.target.identifier !== "ppop")return;
-    try{
-        var long_url = safari.application.activeBrowserWindow.activeTab.url;
-        $.ajax({
-            url:'http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long='+long_url,
-            async:false,
-            data:null,
-            error:function(){
+    var long_url = safari.application.activeBrowserWindow.activeTab.url;
+    $.ajax({
+        url:'http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long='+long_url,
+        async:false,
+        error:function(){
+            $('#qrcode-box').empty();
+            $('#qrcode-box').qrcode({
+                width: 200,
+                height: 200,
+                text: long_url
+            });
+        },
+        success:function(re){
+            try{
+                $('#qrcode-box').empty();
+                $('#qrcode-box').qrcode({
+                    width: 200,
+                    height: 200,
+                    text: re[0].url_short
+                });
+            }catch(e){
                 $('#qrcode-box').empty();
                 $('#qrcode-box').qrcode({
                     width: 200,
@@ -15,16 +29,6 @@ function fff(event){
                     text: long_url
                 });
             }
-            success:function(re){
-                $('#qrcode-box').empty();
-                $('#qrcode-box').qrcode({
-                    width: 200,
-                    height: 200,
-                    text: re[0].url_short
-                });
-            }
-        });
-    }catch(e){
-        alert(e);
-    }
+        }
+    });
 }
